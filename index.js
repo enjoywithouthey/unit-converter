02e046979545ce178f9e3756e5ca9fe184da2192
Justin <justin@surfingwave.com>
2022-08-17 10:13:32 -0400

    Initial Files
    Copied original files from downloaded Scrimba project

---
+++ index.js
@@ -0,0 +1,35 @@
+const inputEl = document.getElementById("input-el")
+const btnEl = document.getElementById("btn-el")
+const lengthEl= document.getElementById("length-el")
+const volumeEl= document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+btnEl.addEventListener("click", function() {
+    let myNum = document.getElementById("input-el").value 
+    convLen(myNum)
+    convVol(myNum)
+    convMa(myNum)
+
+    // Convert Functions
+    function convLen() {
+        let stringLen = `${myNum} meters = ${(myNum * 3.281).toFixed(2)} feet | ${myNum} feet = ${(myNum / 3.281).toFixed(2)} meters`
+        lengthEl.innerHTML = stringLen
+    }
+
+    function convVol() {
+        let stringVol = `${myNum} liters = ${(myNum * 0.264).toFixed(2)} gallons | ${myNum} gallons = ${(myNum / 0.264).toFixed(2)} liters`
+        volumeEl.innerHTML = stringVol
+    }
+
+    function convMa() {
+         let stringMa = `${myNum} kilos = ${(myNum * 2.204).toFixed(2)} pounds | ${myNum} pounds = ${(myNum / 2.204).toFixed(2)} kilos`
+         massEl.innerHTML = stringMa   
+    }
+})
+
+// Given
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
