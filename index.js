43d6b05c903e59ffa8deffd140acc42606a38bdb
Justin <justin@surfingwave.com>
2022-08-17 10:20:51 -0400

    Update index.js
    Capitalized Meters, Liters and Kilos

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ btnEl.addEventListener("click", function() {
 
     // Convert Functions
     function convLen() {
-        let stringLen = `${myNum} meters = ${(myNum * 3.281).toFixed(2)} feet | ${myNum} feet = ${(myNum / 3.281).toFixed(2)} meters`
+        let stringLen = `${myNum} Meters = ${(myNum * 3.281).toFixed(2)} feet | ${myNum} feet = ${(myNum / 3.281).toFixed(2)} meters`
         lengthEl.innerHTML = stringLen
     }
 
     function convVol() {
-        let stringVol = `${myNum} liters = ${(myNum * 0.264).toFixed(2)} gallons | ${myNum} gallons = ${(myNum / 0.264).toFixed(2)} liters`
-        volumeEl.innerHTML = stringVol
+        let stringVol = `${myNum} Liters = ${(myNum * 0.264).toFixed(2)} gallons | ${myNum} gallons = ${(myNum / 0.264).toFixed(2)} liters`
+        volumeEl.innerHTML = strinVol
     }
 
     function convMa() {
-         let stringMa = `${myNum} kilos = ${(myNum * 2.204).toFixed(2)} pounds | ${myNum} pounds = ${(myNum / 2.204).toFixed(2)} kilos`
+         let stringMa = `${myNum} Kilos = ${(myNum * 2.204).toFixed(2)} pounds | ${myNum} pounds = ${(myNum / 2.204).toFixed(2)} kilos`
          massEl.innerHTML = stringMa   
     }
 })
